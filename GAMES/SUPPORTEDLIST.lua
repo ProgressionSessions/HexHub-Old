c076d877f5ec8225299c4f31f5bd8bd93a3da9c9
Oliver Kazik <97758429+MrolivesGaming@users.noreply.github.com>
2023-08-19 20:10:21 -0700

    Update SUPPORTEDLIST.lua

--- GAMES/SUPPORTEDLIST.lua
+++ GAMES/SUPPORTEDLIST.lua
@@ -4,7 +4,7 @@ return {
 		["METADATA"] = {
 			["NAME"] = "Evade",
 			["DESCRIPTION"] = 'Evade, A game where you attempt to evade a series of robotic PNGs, Tap "LOAD HEXHUB" to load the script!',
-			["THUMBNAIL"] = "rbxassetid://11425766633",
+			["THUMBNAIL"] = "rbxassetid://14502082532",
 		},
 		["STATUS"] = {
 			["PATCHED"] = true,
@@ -16,7 +16,19 @@ return {
 		["METADATA"] = {
 			["NAME"] = "Jailbreak",
 			["DESCRIPTION"] = 'Jailbreak, A game where you can escape a prison, aswell as make money via robberys and whatnot, Tap "LOAD HEXHUB" to load the script!',
-			["THUMBNAIL"] = "rbxassetid://9605699097",
+			["THUMBNAIL"] = "rbxassetid://14502087051",
+		},
+		["STATUS"] = {
+			["PATCHED"] = false,
+			["UPDATED"] = false,
+		}
+	},
+	[606849621] = { -- Vehicle simulator
+		["SCRIPT"] = "VEHICLESIMULATOR",
+		["METADATA"] = {
+			["NAME"] = "Vehicle simulator",
+			["DESCRIPTION"] = 'Vehicle Simulattor, A game where you can drive cars, race players and just enjoy the driving experience, Tap "LOAD HEXHUB" to load the script!',
+			["THUMBNAIL"] = "rbxassetid://14502059637",
 		},
 		["STATUS"] = {
 			["PATCHED"] = false,
