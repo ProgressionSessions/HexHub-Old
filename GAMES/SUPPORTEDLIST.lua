dd3702980dd079416caf48a763d961c52d063e63
Oliver Kazik <97758429+MrolivesGaming@users.noreply.github.com>
2023-08-19 18:52:28 -0700

    Update SUPPORTEDLIST.lua

--- GAMES/SUPPORTEDLIST.lua
+++ GAMES/SUPPORTEDLIST.lua
@@ -1,4 +1,26 @@
 return {
-	[9872472334] = "EVADE",
-	[606849621] = "JAILBREAK",
+	[9872472334] = { -- evade
+		["SCRIPT"] = "EVADE",
+		["METADATA"] = {
+			["NAME"] = "Evade",
+			["DESCRIPTION"] = 'Evade, A game where you attempt to evade a series of robotic PNGs, Tap "LOAD HEXHUB" to load the script!',
+			["THUMBNAIL"] = "rbxassetid://11425766633",
+		},
+		["STATUS"] = {
+			["PATCHED"] = true,
+			["UPDATED"] = false,
+		}
+	}, 
+	[606849621] = { -- jailbreak
+		["SCRIPT"] = "JAILBREAK",
+		["METADATA"] = {
+			["NAME"] = "Jailbreak",
+			["DESCRIPTION"] = 'Jailbreak, A game where you can escape a prison, aswell as make money via robberys and whatnot, Tap "LOAD HEXHUB" to load the script!',
+			["THUMBNAIL"] = "rbxassetid://9605699097",
+		},
+		["STATUS"] = {
+			["PATCHED"] = false,
+			["UPDATED"] = false,
+		}
+	},
 }
