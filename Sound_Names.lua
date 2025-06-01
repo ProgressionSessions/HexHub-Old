78c5b1d39f931c9f947f9fa28f5aefb815679904
Oliver Kazik <97758429+ProgressionSessions@users.noreply.github.com>
2025-06-01 14:39:48 -0400

    Add files via upload

---
+++ Sound_Names.lua
@@ -0,0 +1,12 @@
+return {
+	names = {
+		"audio/weapon",
+		"swing",
+		"slash"
+	},
+	ids = {
+		"rbxassetid://9614559376", -- BO Staff 3 (name deleted)
+		"rbxassetid://9614559684",-- BO Staff 2 (name deleted)
+		"rbxassetid://9614559730"-- BO Staff 1 (name deleted)
+	}
+}
